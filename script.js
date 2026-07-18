26080d1b371c9f45828b12221b0d4f2b5addb612
Daniel Lievesley <danieljlievesley@gmail.com>
2026-07-18 16:22:42 +0100

    volunteering page and js added for button functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+let findoutmorebtn = document.getElementById("findmore");
+findoutmorebtn.addEventListener("click", visitabout);
+
+function visitabout(){
+    window.open('about.html', '_self');
+}
